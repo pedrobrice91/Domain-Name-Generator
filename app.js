3dbaa22c2d0fdad15dad7a6861a085cd9b17a629
Pedro L Briceno <66653154+pedrobrice91@users.noreply.github.com>
2024-06-24 17:48:54 -0400

    entrega

---
+++ app.js
@@ -0,0 +1,11 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+for (let i = 0; i < pronoun.length; i++){
+    for(let j = 0; j < adj.length; j++){
+        for(let p = 0; p < noun.length; p++){
+            console.log(`${pronoun[i]} ${adj[j]} ${noun[p]}`)
+        }
+    }
+}
