dadd857b796c8184f6fe22ae7e9673057bd77558
Pedro L Briceno <66653154+pedrobrice91@users.noreply.github.com>
2024-06-27 16:42:22 -0400

    correccion

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
+let fin = ".com"
 
 for (let i = 0; i < pronoun.length; i++){
     for(let j = 0; j < adj.length; j++){
         for(let p = 0; p < noun.length; p++){
-            console.log(`${pronoun[i]} ${adj[j]} ${noun[p]}`)
+            console.log(pronoun[i] + adj[j] + noun[i]+fin)
         }
     }
 }
