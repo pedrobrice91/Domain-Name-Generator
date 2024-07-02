80720bb920f09858f01dca80e4bdd96fe2119a3d
Pedro L Briceno <66653154+pedrobrice91@users.noreply.github.com>
2024-07-01 20:22:02 -0400

    correccion

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let fin = ".com"
 for (let i = 0; i < pronoun.length; i++){
     for(let j = 0; j < adj.length; j++){
         for(let p = 0; p < noun.length; p++){
-            console.log(pronoun[i] + adj[j] + noun[i]+fin)
+            console.log(pronoun[i] + adj[j] + noun[p]+fin)
         }
     }
 }
